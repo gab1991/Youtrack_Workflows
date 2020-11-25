71521c0678c9092e35d9a462d325d2a802d30eeb
Igor.Kornilov <gaber32188@gmail.com>
2020-11-25 14:20:49 +0300

    issue to review on pr blank rule

---
+++ pr_creation_issue_to_review/pr_creatation_issue_to_review.js
@@ -0,0 +1,25 @@
+/**
+ * This is a template for an on-change rule. This rule defines what
+ * happens when a change is applied to an issue.
+ *
+ * For details, read the Quick Start Guide:
+ * https://www.jetbrains.com/help/youtrack/incloud/2020.4/Quick-Start-Guide-Workflows-JS.html
+ */
+
+var entities = require('@jetbrains/youtrack-scripting-api/entities');
+
+exports.rule = entities.Issue.onChange({
+  // TODO: give the rule a human-readable title
+  title: 'Pr_creatation_issue_to_review',
+  guard: function(ctx) {
+    // TODO specify the conditions for executing the rule
+    return true;
+  },
+  action: function(ctx) {
+    var issue = ctx.issue;
+    // TODO: specify what to do when a change is applied to an issue
+  },
+  requirements: {
+    // TODO: add requirements
+  }
+});
