694741233740d148506660ed95f429a586d2c7d7
Igor.Kornilov <gaber32188@gmail.com>
2020-11-25 11:53:39 +0300

    v 1.0

--- pullrequest_accepted/pr_accepted.js
+++ pullrequest_accepted/pr_accepted.js
@@ -1,7 +1,7 @@
 var entities = require('@jetbrains/youtrack-scripting-api/entities');
+var workflow = require('@jetbrains/youtrack-scripting-api/workflow');
 
 exports.rule = entities.Issue.onChange({
-  // TODO: give the rule a human-readable title
   title: 'Pr_accepted',
 
   guard: function (ctx) {
@@ -9,7 +9,6 @@ exports.rule = entities.Issue.onChange({
       ctx.issue.fields.State && // fixing issue.Draft error
       ctx.issue.fields.State.name === ctx.State.Review.name &&
       ctx.issue.pullRequests.isNotEmpty() && // ensure there is PRs
-      //!ctx.issue.fields.isChanged(ctx.State) && // allow users to change State manually, otherwise this will be blocked
       ctx.issue.pullRequests.last().previousState &&
       ctx.issue.pullRequests.last().state.name !==
         ctx.issue.pullRequests.last().previousState.name
@@ -22,13 +21,19 @@ exports.rule = entities.Issue.onChange({
 
     var lastPR = issue.pullRequests.last();
 
-    // Checking if PR is  MERGED and Issue is now under Review. If so, set Issue to "In Progress" state
+    // Checking if PR is  MERGED and Issue is now under Review. If so, set Issue to "Testing" state
     var continueCondition =
       lastPR.state.name === 'MERGED' && issueStatus === ctx.State.Review.name;
 
     if (!continueCondition) {
       return;
     }
+
+    // will throw an error if user tries to move task with merged pr To Review.
+    workflow.check(
+      !ctx.issue.fields.isChanged(ctx.State),
+      'You cannot move issue with the last PR merged to Review State. You should create a new PR to do so'
+    );
 
     //Move to testing
     issue.fields.State = ctx.State.Testing;
