2cd7fb0d13d461f3de1878e6feb9af039514c251
Igor.Kornilov <gaber32188@gmail.com>
2020-12-24 10:21:27 +0300

    new project setup fix

--- pr_creation_issue_to_review/pr_creatation_issue_to_review.js
+++ pr_creation_issue_to_review/pr_creatation_issue_to_review.js
@@ -2,34 +2,36 @@ var entities = require('@jetbrains/youtrack-scripting-api/entities');
 var workflow = require('@jetbrains/youtrack-scripting-api/workflow');
 
 exports.rule = entities.Issue.onChange({
-  title: 'Pr_creatation_issue_to_review',
+	title: 'Pr_creatation_issue_to_review',
 
-  guard: function (ctx) {
-    return (
-      ctx.issue.fields.State && // fixing issue.Draft error
-      ctx.issue.fields.State.name === ctx.State.InProgress.name &&
-      ctx.issue.pullRequests.isNotEmpty() && // ensure there is PRs
-      ctx.issue.pullRequests.last().previousState === null // ensure that this is a new PR added
-    );
-  },
+	guard: function (ctx) {
+		return (
+			ctx.issue.fields.State && // fixing issue.Draft error
+			ctx.issue.fields.State.name === ctx.State.InProgress.name &&
+			ctx.issue.pullRequests.isNotEmpty() && // ensure there is PRs
+			ctx.issue.pullRequests.last().previousState === null // ensure that this is a new PR added
+		);
+	},
 
-  action: function (ctx) {
-    // move issue to review
-    ctx.issue.fields.State = ctx.State.Review;
+	action: function (ctx) {
+		// move issue to review
+		ctx.issue.fields.State = ctx.State.Review;
 
-    workflow.message('Your task has been sent ' + ctx.State.Review.name);
-  },
+		workflow.message('Your task has been sent ' + ctx.State.Review.name);
 
-  requirements: {
-    // Has to match states from current project
-    State: {
-      type: entities.State.fieldType,
-      Review: {
-        name: 'To Review',
-      },
-      InProgress: {
-        name: 'In Progress',
-      },
-    },
-  },
+		console.log('pr_createion_issue_to_review', ctx.issue.id);
+	},
+
+	requirements: {
+		// Has to match states from current project
+		State: {
+			type: entities.EnumField.fieldType,
+			Review: {
+				name: 'Reviewing',
+			},
+			InProgress: {
+				name: 'In Progress',
+			},
+		},
+	},
 });
