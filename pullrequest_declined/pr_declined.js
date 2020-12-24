5eb723d287a28cbde5abcb26ca97c5bd83fa6e38
Igor.Kornilov <gaber32188@gmail.com>
2020-12-24 10:28:55 +0300

    new project setup fix

--- pullrequest_declined/pr_declined.js
+++ pullrequest_declined/pr_declined.js
@@ -2,73 +2,73 @@ var entities = require('@jetbrains/youtrack-scripting-api/entities');
 var workflow = require('@jetbrains/youtrack-scripting-api/workflow');
 
 exports.rule = entities.Issue.onChange({
-  title: 'Pr_declined',
+	title: 'Pr_declined',
 
-  guard: function (ctx) {
-    return (
-      ctx.issue.fields.State && // fixing issue.Draft error
-      ctx.issue.fields.State.name === ctx.State.Review.name &&
-      ctx.issue.pullRequests.isNotEmpty() && // ensure there is PRs
-      ctx.issue.pullRequests.last().previousState &&
-      ctx.issue.pullRequests.last().state.name !==
-        ctx.issue.pullRequests.last().previousState.name
-    );
-  },
+	guard: function (ctx) {
+		return (
+			ctx.issue.fields.State && // fixing issue.Draft error
+			ctx.issue.fields.State.name === ctx.State.Review.name &&
+			ctx.issue.pullRequests.isNotEmpty() && // ensure there is PRs
+			ctx.issue.pullRequests.last().previousState &&
+			ctx.issue.pullRequests.last().state.name !== ctx.issue.pullRequests.last().previousState.name
+		);
+	},
 
-  action: function (ctx) {
-    var issue = ctx.issue;
-    var issueStatus = issue.fields.State.name;
+	action: function (ctx) {
+		var issue = ctx.issue;
+		var issueStatus = issue.fields.State.name;
 
-    var lastPR = issue.pullRequests.last();
-    var lastPRAuthor = lastPR.user;
+		var lastPR = issue.pullRequests.last();
+		var lastPRAuthor = lastPR.user;
 
-    // Checking if PR is Declined and Issue is now under Review. If so, set Issue to "In Progress" state
-    var continueCondition =
-      lastPR.state.name === 'DECLINED' && issueStatus === ctx.State.Review.name;
+		// Checking if PR is Declined and Issue is now under Review. If so, set Issue to "In Progress" state
+		var continueCondition = lastPR.state.name === 'DECLINED' && issueStatus === ctx.State.Review.name;
 
-    if (!continueCondition) {
-      return;
-    }
+		if (!continueCondition) {
+			return;
+		}
 
-    // will throw an error if user tries to move task with declined pr To Review.
-    workflow.check(
-      !ctx.issue.fields.isChanged(ctx.State),
-      'You cannot move issue with the last PR declined to Review State. You should create a new PR to do so'
-    );
+		// will throw an error if user tries to move task with declined pr To Review.
+		workflow.check(
+			!ctx.issue.fields.isChanged(ctx.State),
+			'You cannot move issue with the last PR declined to Review State. You should create a new PR to do so',
+		);
 
-    issue.fields.State = ctx.State.InProgress;
+		issue.fields.State = ctx.State.InProgress;
 
-    var projectUsers = issue.project.team.users;
-    var lastPRAuthorInYT = lastPRAuthor
-      ? projectUsers.find(function (YTuser) {
-          return YTuser.login === lastPRAuthor.login;
-        })
-      : null;
+		var projectUsers = issue.project.team.users;
+		var lastPRAuthorInYT = lastPRAuthor
+			? projectUsers.find(function (YTuser) {
+					return YTuser.login === lastPRAuthor.login;
+			  })
+			: null;
 
-    // Remove rewier from the issue
-    issue.fields.Assignee.clear();
+		// Remove rewier from the issue
+		issue.fields.Assignee.clear();
 
-    // if lastPRAuthorInYT is the author of PR then assign to him. Otherwise leave unassign
-    if (lastPRAuthorInYT) {
-      issue.fields.Assignee.add(lastPRAuthorInYT);
-    }
-  },
+		// if lastPRAuthorInYT is the author of PR then assign to him. Otherwise leave unassign
+		if (lastPRAuthorInYT) {
+			issue.fields.Assignee.add(lastPRAuthorInYT);
+		}
 
-  requirements: {
-    // Has to match states from current project
-    State: {
-      type: entities.State.fieldType,
-      InProgress: {
-        name: 'In Progress',
-      },
-      Review: {
-        name: 'To Review',
-      },
-    },
-    // In order the script to work we must provide info that this custom type exists
-    Assignee: {
-      type: entities.User.fieldType,
-      multi: true,
-    },
-  },
+		console.log('pr_declined', issue.id);
+	},
+
+	requirements: {
+		// Has to match states from current project
+		State: {
+			type: entities.EnumField.fieldType,
+			InProgress: {
+				name: 'In Progress',
+			},
+			Review: {
+				name: 'Reviewing',
+			},
+		},
+		// In order the script to work we must provide info that this custom type exists
+		Assignee: {
+			type: entities.User.fieldType,
+			multi: true,
+		},
+	},
 });
