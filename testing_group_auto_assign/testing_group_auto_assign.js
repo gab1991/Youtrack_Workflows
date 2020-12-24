eba90cea3c07becf134da59be69ecde092d3f801
Igor.Kornilov <gaber32188@gmail.com>
2020-12-24 12:08:51 +0300

    testing auto assign v1.0

--- testing_group_auto_assign/testing_group_auto_assign.js
+++ testing_group_auto_assign/testing_group_auto_assign.js
@@ -1,25 +1,55 @@
-/**
- * This is a template for an on-change rule. This rule defines what
- * happens when a change is applied to an issue.
- *
- * For details, read the Quick Start Guide:
- * https://www.jetbrains.com/help/youtrack/incloud/2020.6/Quick-Start-Guide-Workflows-JS.html
- */
-
 var entities = require('@jetbrains/youtrack-scripting-api/entities');
+var workflow = require('@jetbrains/youtrack-scripting-api/workflow');
 
 exports.rule = entities.Issue.onChange({
-  // TODO: give the rule a human-readable title
-  title: 'Testing_group_auto_assign',
-  guard: function(ctx) {
-    // TODO specify the conditions for executing the rule
-    return true;
-  },
-  action: function(ctx) {
-    var issue = ctx.issue;
-    // TODO: specify what to do when a change is applied to an issue
-  },
-  requirements: {
-    // TODO: add requirements
-  }
+	title: 'testing_group_auto_assign',
+
+	guard: function (ctx) {
+		return (
+			ctx.issue.fields.State && // fixing issue.Draft error
+			ctx.issue.fields.State.name === ctx.State.Testting.name &&
+			ctx.issue.fields.Assignee &&
+			// should be unassign in order to not blocking manual assignment
+			ctx.issue.fields.Assignee.isEmpty()
+		);
+	},
+
+	action: function (ctx) {
+		function getRandomWithLimit(limit) {
+			return Math.floor(Math.random() * Math.floor(limit));
+		}
+
+		var isTestingGroupEmpty = ctx.TestingGroup.users.isEmpty();
+
+		if (isTestingGroupEmpty) {
+			workflow.message(
+				'Add someone to "' + ctx.TestingGroup.name + '" group if you wish auto assignment, or disable the workflow',
+			);
+			return;
+		}
+
+		var testingGroupUsersSize = ctx.TestingGroup.users.size;
+		var index = getRandomWithLimit(testingGroupUsersSize);
+		var randomUser = ctx.TestingGroup.users.get(index);
+
+		ctx.issue.fields.Assignee.add(randomUser);
+	},
+
+	requirements: {
+		// Has to match states from current project
+		State: {
+			type: entities.EnumField.fieldType,
+			Testting: {
+				name: 'Testing',
+			},
+		},
+		Assignee: {
+			type: entities.User.fieldType,
+			multi: true,
+		},
+		TestingGroup: {
+			type: entities.UserGroup,
+			name: 'Testing Assignees',
+		},
+	},
 });
